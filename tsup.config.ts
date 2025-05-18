85b0a285a5d8f7571bb974627195883391dbf9c9
Victoria Rodriguez <vrodriguezfe@icloud.com>
2025-05-18 14:46:56 -0400

    :wrench: (build) Setup configuration with new files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,28 +1,51 @@
-import { defineConfig } from "tsup";
+import { defineConfig, Options } from "tsup";
+
+const common = {
+  bundle: true,
+  entry: ["source/bin/setup-linting.ts"],
+  format: ["esm"],
+  platform: "node",
+  shims: true,
+  splitting: false,
+  target: "esnext",
+  tsconfig: "tsconfig.bundle.json",
+  treeshake: {
+    preset: "smallest",
+    moduleSideEffects: "no-external",
+    correctVarValueBeforeDeclaration: true,
+  },
+} satisfies Options;
+
+const library = {
+  banner: {
+    js: "// @ts-nocheck\n/* eslint-disable */\n",
+  },
+  dts: {
+    only: false,
+    banner: "// @ts-nocheck\n/* eslint-disable */\n",
+    resolve: true,
+  },
+  entry: ["source/index.ts"],
+  name: "library",
+  outDir: "lib",
+} satisfies Options;
+
+const binaries = {
+  banner: {
+    js: "#!/usr/bin/env node\n// @ts-nocheck\n/* eslint-disable */\n",
+  },
+  dts: false,
+  name: "binaries",
+  outDir: "bin",
+} satisfies Options;
 
 export default defineConfig([
   {
-    banner: {
-      js: "// @ts-nocheck\n/* eslint-disable */\n",
-    },
-    bundle: true,
-    dts: {
-      only: false,
-      banner: "// @ts-nocheck\n/* eslint-disable */\n",
-      resolve: true,
-    },
-    entry: ["source/main.ts"],
-    format: ["esm", "cjs"],
-    outDir: "lib",
-    platform: "node",
-    shims: true,
-    splitting: false,
-    target: "esnext",
-    tsconfig: "tsconfig.bundle.json",
-    treeshake: {
-      preset: "smallest",
-      moduleSideEffects: "no-external",
-      correctVarValueBeforeDeclaration: true,
-    },
+    ...common,
+    ...library,
+  },
+  {
+    ...common,
+    ...binaries,
   },
 ]);
